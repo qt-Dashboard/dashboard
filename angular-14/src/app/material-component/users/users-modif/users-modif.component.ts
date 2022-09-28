48c2d6cea3938e5c48d6b5e67cd9229ac085a7dd
Quentin <quentin.garreau72@gmail.com>
2022-09-28 11:06:45 +0200

    Ajout du projet -Q

---
+++ angular-14/src/app/material-component/users/users-modif/users-modif.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-users-modif',
+  templateUrl: './users-modif.component.html',
+  styleUrls: ['./users-modif.component.css']
+})
+export class UsersModifComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

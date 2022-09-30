8dd409470c78abf640fe6eb86826b504871bfe0f
Thomas <t.audouin@gmail.com>
2022-09-30 15:52:59 +0200

    màj Table -T

--- angular-14/src/app/material-component/users/users.component.ts
+++ angular-14/src/app/material-component/users/users.component.ts
@@ -2,27 +2,51 @@ import { Component, Inject, OnInit } from "@angular/core";
 import { MatDialog } from "@angular/material/dialog";
 import { Router } from "@angular/router";
 import { UsersService } from "src/app/services/users.service";
-import { DialogConfirmComponent } from "./dialog-confirm/dialog-confirm.component";
 import { MatSnackBar, MatSnackBarHorizontalPosition, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
-import { User } from "src/app/models/user.model";
+
+export interface User {
+  id?: string;
+  name?: string;
+  password?: string;
+  email: string;
+  phone?: string;
+  token?: string;
+  isAdmin?: boolean;
+  street?: string;
+  apartment?: string;
+  zip?: string;
+  city?: string;
+  country?: string;
+}
 
 @Component({
   selector: "my-app",
   templateUrl: "./users.component.html",
   styleUrls: ["./users.component.css"]
 })
+
 export class UsersComponent implements OnInit{
   users: User[] = [];
+  displayedColumns: string[] = ['name', 'email', 'isAdmin', 'country', 'iconUpd', 'iconDel'];
 
   horizontalPosition: MatSnackBarHorizontalPosition = 'end';
   verticalPosition: MatSnackBarVerticalPosition = 'top';
+
 
   constructor(
     public dialog: MatDialog,
     private snackBar: MatSnackBar,
     private usersService: UsersService,
-    private router: Router
-    ) {}
+    private router: Router,
+    private service: UsersService
+    ) {
+      this.service.getUsers().subscribe(users => {
+      this.users = users;
+        });
+  }
+
+
+  
 
     ngOnInit(): void {
       this.getUsers();

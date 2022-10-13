755adbb6cc22746985c2b1cee77cd584bb062449
Quentin <quentin.garreau72@gmail.com>
2022-10-13 16:17:35 +0200

    Maj Map

--- angular-14/src/app/services/map.service.ts
+++ angular-14/src/app/services/map.service.ts
@@ -18,7 +18,7 @@ export class MapService {
 
 
   constructor(private http: HttpClient) {
-    
+
   }
 
 
@@ -28,96 +28,72 @@ export class MapService {
 
       for (const c of res) {
         const lat = c.coordinates[0];
-        const lon = c.coordinates[1];          
+        const lon = c.coordinates[1];
         var routeOn = false;
+
         var greenIcon = L.icon({ //add this new icon
 
           iconUrl: c.iconUrl,
           shadowUrl: './assets/images/map/marker-shadow.png',
 
-          iconAnchor:   [12, 40], // point of the icon which will correspond to marker's location
+          iconAnchor: [12, 40], // point of the icon which will correspond to marker's location
           shadowAnchor: [12, 40],  // the same for the shadow
-          popupAnchor:  [0, -76] // point from which the popup should open relative to the iconAnchor
+          popupAnchor: [0, -76] // point from which the popup should open relative to the iconAnchor
 
-      });
+        });
 
-      
-      
+
+
         let marker = L.marker([lat, lon]).addTo(map);
-        marker.bindPopup(`<center>
-        <p>
-        <strong>${c.name}</strong>
-        </p>
-        </center>
-        
-        <script>
-          function test(){
-            alert("ok");
-          }
-        </script>
-
-        <button onclick="goTo(${c.coordinates[0], c.coordinates[1], 1, '', ''})" >ICI</button>
-        <p>${c.description}</p>
-        `);
+        // marker.bindPopup(`<body><center>
+        // <p>
+        // <strong>${c.name}</strong>
+        // </p>
+        // </center>
+        // <button onclick="goTo(${c.coordinates[0], c.coordinates[1], 1, '', ''})" >ICI</button>
+        // <p>${c.description}</p>
+        //         <script>
+        //   function test(){
+        //     alert("ok");
+        //   }
+        // </script>
+        // </body>`);
         marker.setIcon(greenIcon);
-        // marker.on('click', function(){
+      
 
-        // alert("!");
+        // function market() {
 
-        
-    //     marker.on('click', function () {
-    //       if (routeOn === false) {
-            
-          
-    //       L.Routing.control({
-    //       router: L.Routing.osrmv1({
-    //         serviceUrl: `https://router.project-osrm.org/route/v1/`
-    //       }),
-    //       routeWhileDragging: true,
-    //       showAlternatives: true,
-    //       fitSelectedRoutes: false,
-    //       show: false,
-    //       addWaypoints: false,
-        
-    //       waypoints: [
-    //         L.latLng(c.coordinates[0], c.coordinates[1], 1),
-    //         L.latLng(10, 40)
-    //       ],
-          
-          
-    //     }).addTo(map); routeOn = true;
-    //     }else{routeOn = true;
-    //       L.Routing.control({
-    //         router: L.Routing.osrmv1({
-    //           serviceUrl: `http://router.project-osrm.org/route/v1/`
-    //         }),
-    //         routeWhileDragging: true,
-    //         showAlternatives: true,
-    //         fitSelectedRoutes: false,
-    //         show: false,
-    //         addWaypoints: false,
-          
-    //         waypoints: [
-    //           L.latLng(c.coordinates[0], c.coordinates[1], 1),
-    //           L.latLng(10, 40)
-    //         ], }).remove();
-    //        routeOn = false;}  })
-    // // });
 
-    
+        //   L.Routing.control({
+
+        //     router: L.Routing.osrmv1({
+        //       serviceUrl: `http://router.project-osrm.org/route/v1/`
+        //     }),
+        //     routeWhileDragging: true,
+        //     showAlternatives: true,
+        //     fitSelectedRoutes: false,
+        //     show: false,
+        //     addWaypoints: false,
+        //     waypoints: [
+        //       L.latLng(lat, lon),
+        //       L.latLng(5, 10)
+        //     ],
+
+
+
+        //   }).addTo(map);
+        // }
       }
-    }); 
-  
+    })
   }
 
-//   const button = document.getElementById('test1');
+  //   const button = document.getElementById('test1');
 
-//   button?.addEventListener('click', function handleClick(event) {
-//   console.log('button clicked');
-//   console.log(event);
-//   console.log(event.target);
-// });
-
+  //   button?.addEventListener('click', function handleClick(event) {
+  //   console.log('button clicked');
+  //   console.log(event);
+  //   console.log(event.target);
+  // });
 
 
 
